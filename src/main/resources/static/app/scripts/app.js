ded4053db3a003bbd8790f8afa37e295795c482f
Leandro Santana <iileandro@gmail.com>
2023-10-09 03:19:27 -0300

    Frontend issues. Listing in tree.

--- src/main/resources/static/app/scripts/app.js
+++ src/main/resources/static/app/scripts/app.js
@@ -1,18 +1,43 @@
 (function intApplication(){
     const app = angular.module('app', []);
     app.controller('list-control', listController);
+    app.directive('collabTreeNode', collabTreeNode);
 })();
 
 
 function listController($scope, $http) {
-    $scope.titulo = 'Page Title';
+    $scope.title = 'Listing ';
     $http.get('http://localhost:8080/api/v1/collaborator')
-        .then(function(response) {
+        .then(function (response) {
             // Atribuir a resposta (lista de itens) ao escopo
-            $scope.lista = response.data;
+            $scope.list = response.data;
         })
-        .catch(function(error) {
+        .catch(function (error) {
             alert('Erro ao carregar a lista de itens.');
             console.log(error);
         });
+}
+
+function collabTreeNode() {
+    return {
+        restrict: 'E',
+        scope: {
+            node: '='
+        },
+        template:
+        `<li>
+                {{ node.name }}  <button ng-show="node.managedCollaborators.length" ng-click="toggleChildren()">+/-</button>
+                <span class="percent-box">{{ node.passwordScore }}%</span> 
+                <span class="complexity-box" ng-class="'cb-'+node.passComplexity.id">{{ node.passComplexity.label}}</span> 
+                <ul ng-show="node.childrenVisible">
+                    <collab-tree-node ng-repeat="child in node.managedCollaborators" node="child"></collab-tree-node>
+                </ul>
+        </li>`,
+        link: function (scope) {
+            scope.node.childrenVisible = false;
+            scope.toggleChildren = function () {
+                scope.node.childrenVisible = !scope.node.childrenVisible;
+            };
+        }
+    };
 }
