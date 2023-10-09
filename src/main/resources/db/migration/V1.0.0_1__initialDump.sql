9200486c17dc21b15d27819eb47d1aee93aebcd4
Leandro Santana <iileandro@gmail.com>
2023-10-08 22:50:19 -0300

    mysql included.

---
+++ src/main/resources/db/migration/V1.0.0_1__initialDump.sql
@@ -0,0 +1,21 @@
+-- version: 10.2.14-MariaDB (Mysql)
+-- innodb_version: 5.7.21
+CREATE DATABASE `fpcolabman`CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+USE fpcolabman;
+
+CREATE TABLE collaborator
+(
+    `id`                 INTEGER NOT NULL AUTO_INCREMENT,
+    `name`               VARCHAR(255),
+    `encrypted_password` VARCHAR(255),
+    `password_score`     INTEGER NOT NULL,
+    `tree_path`          LONGTEXT,
+    `manager_id`         INTEGER,
+     PRIMARY KEY (id)
+) ENGINE=INNODB;
+
+ALTER TABLE collaborator
+    ADD CONSTRAINT FKptah6kogyx6mccw1lbvd9mltd
+    FOREIGN KEY (manager_id)
+    REFERENCES collaborator (id);
